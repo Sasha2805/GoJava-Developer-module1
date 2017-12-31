573cb4b3d37a4024571092b02e2753d7ddc49c7b
Sasha Kaminska <sasha249133@ukr.net>
2017-12-31 18:52:55 +0200

    added a field "salary" to the developers table

---
+++ homework1.2/1.sql
@@ -0,0 +1,23 @@
+# Добавить разработчикам поле (salary - зарплата).
+USE development;
+
+ALTER TABLE developers ADD salary DECIMAL(10, 2);
+
+CREATE TABLE developers_salary(
+	developer_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	salary DECIMAL(10, 2)
+);
+
+INSERT INTO developers_salary(developer_id, salary) 
+VALUES
+	(1, 4000),
+	(2, 1000),
+	(3, 3500),
+	(4, 2500.80),
+	(5, 3100);
+
+UPDATE developers, developers_salary 
+SET developers.salary = developers_salary.salary
+WHERE id = developers_salary.developer_id;
+
+DROP TABLE developers_salary;
