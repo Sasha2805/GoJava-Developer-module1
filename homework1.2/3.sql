636284c710d91ecd459015357dee7e48d2de3112
Sasha Kaminska <sasha249133@ukr.net>
2018-01-05 23:36:53 +0200

    modified

--- homework1.2/3.sql
+++ homework1.2/3.sql
@@ -4,4 +4,6 @@ USE development;
 SELECT SUM(developers.salary) AS salary_java_developers
 FROM developers, skills, developer_skills
 WHERE (developers.id = developer_skills.developer_id AND skills.id = developer_skills.skill_id) 
-AND developer_skills.skill_id IN (SELECT id FROM skills WHERE skills.branch_development = 'JAVA');
+AND developer_skills.skill_id IN (SELECT id 
+									FROM skills 
+									WHERE skills.branch_development = 'Java');
