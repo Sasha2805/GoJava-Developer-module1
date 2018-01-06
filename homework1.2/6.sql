ea17c89660f621b1990f9d6eaecf6b0ceb43bd01
Sasha Kaminska <sasha249133@ukr.net>
2018-01-06 17:22:09 +0200

    init commit

--- homework1.2/6.sql
+++ homework1.2/6.sql
@@ -5,6 +5,6 @@ SELECT AVG(salary) AS average_salary
 FROM developers, developer_projects
 WHERE developers.id = developer_projects.developer_id 
 AND project_id = (SELECT id 
-		FROM projects 
-		WHERE cost = (SELECT MIN(cost) AS min_project_cost FROM projects)
-);
+			FROM projects 
+			WHERE cost = (SELECT MIN(cost) AS min_project_cost 
+			FROM projects));
