9bf9233db98d2a61592e9ae09a1498db7b8aab10
Sasha Kaminska <sasha249133@ukr.net>
2018-01-06 17:18:39 +0200

    found average salary

---
+++ homework1.2/6.sql
@@ -0,0 +1,10 @@
+#6. Вычислить среднюю ЗП программистов в самом дешевом проекте.
+USE development;
+
+SELECT AVG(salary) AS average_salary
+FROM developers, developer_projects
+WHERE developers.id = developer_projects.developer_id 
+AND project_id = (SELECT id 
+		FROM projects 
+		WHERE cost = (SELECT MIN(cost) AS min_project_cost FROM projects)
+);
